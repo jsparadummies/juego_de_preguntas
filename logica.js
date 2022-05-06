113ba094e11492b41c92cd9998417ba8c2efaacb
gregorio velandia <gregoriojesusvelandiaurbina@gmail.com>
2022-05-06 05:49:32 -0500

    agrege modales

--- logica.js
+++ logica.js
@@ -1,4 +1,4 @@
-
+//logica de botones contenedores de respuestas//
 (function () {
     const contenedor = document.getElementById('contenedor_de_ventana');
     const botoncito = document.querySelectorAll("#contenedor_de_respuestas button");
@@ -8,5 +8,18 @@
     });
 
 })();
-        
+
+// logica de boton de si//
+ var contenedor_modal = document.getElementById('padre-de-modal');
+ var btnsi = document.getElementById('btnsi');
+ btnsi.addEventListener('click',()=>{
+    contenedor_modal.classList.add('active');
+ });
+
+ //logica de boton de no//
+var modal_perdedor = document.getElementById('padre-de-modal-no');
+ var btnno = document.getElementById('btn_no');
+ btnno.addEventListener('click',()=>{
+    modal_perdedor.classList.add('active');
+ });
     
